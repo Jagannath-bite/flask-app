885f30e443a3b836003ce19d38016bd13699fac3
Jagannath R <65921077+Jagannath-bite@users.noreply.github.com>
2026-06-24 17:01:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "I will update you everytime"
+    return "I will update you everytime44"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
